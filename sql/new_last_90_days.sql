bd0b923d710c1aef76f20721eabe39189b58ec37
Jason Motylinski <jason@motylinski.com>
2023-04-26 20:33:01 -0400

    fix: REVERT

--- sql/new_last_90_days.sql
+++ sql/new_last_90_days.sql
@@ -9,21 +9,22 @@ WITH new_songs_by_day_of_week_hour(day_of_week, hour, ct) AS (
         SELECT 
             artist, 
             title, 
-            MIN(played_at) as played_at
+            played_at, 
+            DENSE_RANK() OVER (
+            PARTITION BY artist, title
+            ORDER BY played_at ASC) AS rank
         FROM songs
-        WHERE 
-            trim(artist) != ''
-            AND trim(title) != ''
-            AND played_at >= Date('now', '-90 days')
-            AND played_at <= Date('now')
-        GROUP BY
-            artist,
-            title
+        WHERE trim(artist) != ''
+        AND trim(title) != ''
     ) b
     ON 
         a.artist=b.artist
         AND a.title=b.title
         AND a.played_at=b.played_at
+    WHERE  
+        b.rank=1
+        AND b.played_at >= Date('now', '-90 days')
+        AND b.played_at <= Date('now')
     GROUP BY 
         a.day_of_week,
         a.hour
