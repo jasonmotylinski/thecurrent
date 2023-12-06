016d4206e8861956386037a3d3d7d9ba54af949e
Jason Motylinski <jason@motylinski.com>
2023-12-06 18:59:15 -0500

    fix: updated parsing logic

--- pipelines/thecurrent/playlist.py
+++ pipelines/thecurrent/playlist.py
@@ -17,11 +17,30 @@ def create_id(played_at, artist, title):
 def get_songs(html):
     """Get the articles for a given year, month, day, hour."""
     bs=BeautifulSoup(html, "html.parser")
-    data=json.loads(bs.find("div", {"class":"playlist-card"}).string)
-    for s in data["props"]["pageProps"]["data"]["songs"]:
-        s["id"] = create_id(s)
-        s["played_at_dt"] = parser.parse(s["played_at"])
-        yield(s)
+    data=bs.find_all("li", {"class": "playlist-card"})
+    dt=bs.find("input", {"name": "playlistDate"}).attrs['value']
+
+    for c in data:
+        artist_album = c.find_all("div", {"class": "playlist-artist"})
+        artist = ''
+        if len(artist_album) > 0:
+             artist = artist_album[0].text
+        album = ''
+        if len(artist_album) == 2:
+            album = artist_album[1].text
+
+        playlist_time = c.find("div", {"class": "playlist-time"}).text
+        playlist_datetime = parser.parse(dt + " " + playlist_time + " -5:00")
+        item = {
+            "title": c.find("h4").text,
+            "artist": artist,
+            "album":  album,
+            "played_at": playlist_datetime.isoformat()
+        }
+        
+        item["id"] = create_id(item['played_at'], item['artist'], item['title'])
+        item["played_at_dt"] = playlist_datetime
+        yield(item)
 
 def get_hour_html(year:int, month:int, day:int, hour:int):
     url=config.THECURRENT_HOUR_URL.format(year=year, month=month, day=day, hour=hour)
