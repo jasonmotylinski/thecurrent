cd036c5819d16b7e9674fc27cc0d1b5017a47e65
Jason Motylinski <jason@motylinski.com>
2024-02-29 10:02:35 -0500

    feature: move timeseries over to postgres

--- sql/popular_all_time_timeseries.sql
+++ sql/popular_all_time_timeseries.sql
@@ -1,18 +1,7 @@
 SELECT 
-    artist, 
-    year,
-    month,
-    year || "-" || month AS year_month, 
-    COUNT(*) as ct 
-FROM songs 
-WHERE artist IN(
-    SELECT artist
-    FROM songs
-    WHERE service_id=1 
-    GROUP BY artist
-    ORDER BY COUNT(*) DESC
-    LIMIT 5
-)
-AND service_id=1
-GROUP BY artist, year, month
-ORDER BY year, month ASC
+    *
+FROM 
+    songs_alltime_top_timeseries 
+ORDER BY 
+    year, 
+    month ASC
