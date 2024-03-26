77b0cf12520ac760590faeaa10cc3967df0a6ae8
Jason Motylinski <jason@motylinski.com>
2024-03-26 19:55:26 -0400

    fix: wrong name for spotify playlist

--- playlists.py
+++ playlists.py
@@ -5,7 +5,7 @@ from rawkit_playlist import playlist
 def update_popular_artist_title_last_week_playlist():
     artist_titles = json.loads(data.get_popular_artist_title_last_week().to_json(orient='table'))['data']
     playlist_data=[{"artist_name": at['artist'], "song_name": at['title']} for at in artist_titles]
-    playlist.update_playlist("Rawk-it - KCRW - Weekly Top 10", "", playlist_data)
+    playlist.update_playlist("Rawk-it - KCMP - Weekly Top 10", "", playlist_data)
 
 
 if __name__=="__main__": 
