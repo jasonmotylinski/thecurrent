c05654d5616309de6eb5eb063f12d8b70eab5bf1
Jason Motylinski <jason@motylinski.com>
2024-03-26 21:06:00 -0400

    feature: generate playlists for all services

--- playlists.py
+++ playlists.py
@@ -1,12 +1,17 @@
+import config
 import json
 from dashboard import data
 from rawkit_playlist import playlist
 
-def update_popular_artist_title_last_week_playlist():
-    artist_titles = json.loads(data.get_popular_artist_title_last_week().to_json(orient='table'))['data']
+def update_popular_artist_title_last_week_playlist(service_id, service_name):
+    artist_titles = json.loads(data.get_popular_artist_title_last_week(service_id=service_id).to_json(orient='table'))['data']
     playlist_data=[{"artist_name": at['artist'], "song_name": at['title']} for at in artist_titles]
-    playlist.update_playlist("Rawk-it - KCMP - Weekly Top 10", "", playlist_data)
+    playlist.update_playlist("Rawk-it - {0} - Weekly Top 10".format(service_name.upper()), "", playlist_data)
 
 
 if __name__=="__main__": 
-    update_popular_artist_title_last_week_playlist()
+    update_popular_artist_title_last_week_playlist(config.THECURRENT.SERVICE_ID, config.THECURRENT.SERVICE_NAME)
+    update_popular_artist_title_last_week_playlist(config.KEXP.SERVICE_ID, config.KEXP.SERVICE_NAME)
+    update_popular_artist_title_last_week_playlist(config.KUTX.SERVICE_ID, config.KUTX.SERVICE_NAME)
+    update_popular_artist_title_last_week_playlist(config.WXPN.SERVICE_ID, config.WXPN.SERVICE_NAME)
+    update_popular_artist_title_last_week_playlist(config.WFUV.SERVICE_ID, config.WFUV.SERVICE_NAME)
