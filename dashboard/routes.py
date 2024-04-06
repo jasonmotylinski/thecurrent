7d7c15a3c811824cc8b2cc754f5abb132526cf13
Jason Motylinski <jason@motylinski.com>
2024-04-06 04:58:55 -0400

    fix: Add service to API

--- dashboard/routes.py
+++ dashboard/routes.py
@@ -1,29 +1,30 @@
+import config
 import data
 
 from flask import Blueprint
 
 api_routes = Blueprint('routes', __name__)
 
-@api_routes.route('/api/popular/last_week/artist_title')
-def get_popular_artist_title_last_week():
-    return data.get_popular_artist_title_last_week().to_json()
+@api_routes.route('/api/<service_name>/popular/last_week/artist_title')
+def get_popular_artist_title_last_week(service_name):
+    return data.get_popular_artist_title_last_week(config.SERVICES[service_name].SERVICE_ID).to_json()
 
-@api_routes.route('/api/popular/last_week/artist')
-def get_popular_artist_last_week():
-    return data.get_popular_artist_last_week().to_json()
+@api_routes.route('/api/<service_name>/popular/last_week/artist')
+def get_popular_artist_last_week(service_name):
+    return data.get_popular_artist_last_week(config.SERVICES[service_name].SERVICE_ID).to_json()
 
-@api_routes.route('/api/new/last_90_days')
-def get_new_last_90_days():
-    return data.get_new_last_90_days().to_json()
+@api_routes.route('/api/<service_name>/new/last_90_days')
+def get_new_last_90_days(service_name):
+    return data.get_new_last_90_days(config.SERVICES[service_name].SERVICE_ID).to_json()
 
-@api_routes.route('/api/popular/all_time/artist_timeseries')
-def get_popular_all_time_timeseries():
-    return data.get_popular_all_time_timeseries().to_json()
+@api_routes.route('/api/<service_name>/popular/all_time/artist_timeseries')
+def get_popular_all_time_timeseries(service_name):
+    return data.get_popular_all_time_timeseries(config.SERVICES[service_name].SERVICE_ID).to_json()
 
-@api_routes.route('/api/popular/all_time/artist')
-def get_popular_all_time():
-    return data.get_popular_all_time().to_json()
+@api_routes.route('/api/<service_name>/popular/all_time/artist')
+def get_popular_all_time(service_name):
+    return data.get_popular_all_time(config.SERVICES[service_name].SERVICE_ID).to_json()
 
-@api_routes.route('/api/popular/all_time/<day_of_week>/<hour>')
-def get_popular_day_hour_data(day_of_week, hour):
-    return data.get_popular_day_hour_data(hour, day_of_week).to_json()
+@api_routes.route('/api/<service_name>/popular/all_time/<day_of_week>/<hour>')
+def get_popular_day_hour_data(service_name, day_of_week, hour):
+    return data.get_popular_day_hour_data(config.SERVICES[service_name].SERVICE_ID, hour, day_of_week).to_json()
--- scripts/prod/cache.sh
+++ scripts/prod/cache.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-curl https://www.rawk-it.com/api/popular/last_week/artist_title
-curl https://www.rawk-it.com/api/popular/last_week/artist
-curl https://www.rawk-it.com/api/new/last_90_days
-curl https://www.rawk-it.com/api/popular/all_time/artist_timeseries
-curl https://www.rawk-it.com/api/popular/all_time/artist
-curl https://www.rawk-it.com/api/popular/all_time/Wednesday/12
+curl https://www.rawk-it.com/api/kcmp/popular/last_week/artist_title
+curl https://www.rawk-it.com/api/kcmp/popular/last_week/artist
+curl https://www.rawk-it.com/api/kcmp/new/last_90_days
+curl https://www.rawk-it.com/api/kcmp/popular/all_time/artist_timeseries
+curl https://www.rawk-it.com/api/kcmp/popular/all_time/artist
+curl https://www.rawk-it.com/api/kcmp/popular/all_time/Wednesday/12
