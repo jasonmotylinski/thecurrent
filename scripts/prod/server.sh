ee45aa5a810b0133959ed9a272688da60a1b2eb5
Jason Motylinski <jason@motylinski.com>
2024-11-03 13:57:49 -0500

    fix: increase workers

--- scripts/prod/server.sh
+++ scripts/prod/server.sh
@@ -1,3 +1,3 @@
 source venv/bin/activate
 export PYTHONPATH=dashboard/.
-gunicorn --workers 2 --log-level DEBUG --bind unix:/run/thecurrent.sock dashboard.app:server
+gunicorn --workers 3 --log-level DEBUG --bind unix:/run/thecurrent.sock dashboard.app:server
