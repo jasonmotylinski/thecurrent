833d59246f648a986af1e97dec3dd65eea1a70b8
Jason Motylinski <jason@motylinski.com>
2025-03-23 08:18:18 -0400

    feature: Added server01 scripts

---
+++ scripts/server01/run_daily_pipeline.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/jason/code/thecurrent
+export PYTHONPATH=.
+source /home/jason/code/thecurrent/venv/bin/activate
+luigi --module pipelines.thecurrent.db_tasks KCMPBackfillLastXDaysData --last-x-days=7 --local-scheduler
+luigi --module pipelines BackfillLastXDaysData --last-x-days=7 --service-name=kexp --local-scheduler
+luigi --module pipelines BackfillLastXDaysData --last-x-days=7 --service-name=kutx --local-scheduler
+luigi --module pipelines BackfillLastXDaysData --last-x-days=7 --service-name=wfuv --local-scheduler
+luigi --module pipelines BackfillLastXDaysData --last-x-days=7 --service-name=wxpn --local-scheduler
+luigi --module pipelines BackfillLastXDaysData --last-x-days=7 --service-name=kcrw --local-scheduler
+luigi --module pipelines BackfillLastXDaysData --last-x-days=7 --service-name=kuom --local-scheduler
---
+++ scripts/server01/run_playlists_live.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+
+cd /home/jason/code/thecurrent
+source /home/jason/code/thecurrent/venv/bin/activate
+python playlist_kcmp_last100played.py
