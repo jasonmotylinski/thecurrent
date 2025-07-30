44b395c53e24c0bac73ce6c0c0d6cefa9767d090
Jason Motylinski <jason@motylinski.com>
2025-07-30 18:36:02 -0400

    fix: performance changes

--- scripts/prod/server.sh
+++ scripts/prod/server.sh
@@ -1,3 +1,3 @@
 source venv/bin/activate
 export PYTHONPATH=.
-gunicorn --workers 3 --log-level DEBUG --bind unix:/run/thecurrent.sock dashboard.server:app
+gunicorn --workers 7 --worker-class gevent --log-level DEBUG --bind unix:/run/thecurrent.sock dashboard.server:app
