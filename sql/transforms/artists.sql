b5a480bfdc020920e6da9f0c4cf201db759ead28
Jason Motylinski <jason@motylinski.com>
2025-12-21 16:32:17 -0500

    feature: artists

---
+++ sql/transforms/artists.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE postgres.artists AS 
+SELECT
+    s.artist,
+    SUM(s.ct) as total_plays
+FROM songs_day_of_week_hour s
+WHERE
+    s.artist != ''
+GROUP BY s.artist
+ORDER BY total_plays DESC;
+
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_artist_trgm
+ON artists USING GIN (artist gin_trgm_ops);
