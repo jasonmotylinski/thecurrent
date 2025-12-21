effe5e6b7789ebab92d7ee8127cff2f35dfd39a7
Jason Motylinski <jason@motylinski.com>
2025-12-21 16:53:42 -0500

    feature: change search to use artists table

--- sql/search.sql
+++ sql/search.sql
@@ -1,10 +1,9 @@
 SELECT
     s.artist,
-    SUM(s.ct) as total_plays
-FROM songs_day_of_week_hour s
+    total_plays
+FROM artists s
 WHERE
     s.artist ILIKE '%(search_term)s'
     AND s.artist != ''
-GROUP BY s.artist
 ORDER BY total_plays DESC
 LIMIT 10
