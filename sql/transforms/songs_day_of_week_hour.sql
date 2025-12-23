85ad6a3a57513cf5e662baaeadbb29b2140ad418
Jason Motylinski <jason@motylinski.com>
2025-12-23 13:16:50 -0500

    fix: add an artist_lower column

--- sql/transforms/songs_day_of_week_hour.sql
+++ sql/transforms/songs_day_of_week_hour.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE TABLE postgres.songs_day_of_week_hour AS
 SELECT 
     CAST(service_id AS INT) as service_id,
     artist, 
+    LOWER(artist) AS artist_lower,
     title,
     CAST(played_at AS DATE) as played_at,
     CAST(year AS INT) as year,
@@ -25,4 +26,8 @@ GROUP BY
 
 CREATE INDEX IF NOT EXISTS service_title_played_at_idx ON postgres.songs_day_of_week_hour (service_id, artist, title, played_at);
 CREATE INDEX IF NOT EXISTS service_artist_title_idx ON postgres.songs_day_of_week_hour (service_id, artist, title);
+
+-- Exclusives query indexes
 CREATE INDEX IF NOT EXISTS played_at_service_artist_idx ON postgres.songs_day_of_week_hour (played_at, service_id, artist);
+CREATE INDEX IF NOT EXISTS played_at_artist_service_idx ON postgres.songs_day_of_week_hour (played_at, artist, service_id);
+CREATE INDEX IF NOT EXISTS played_at_artist_lower_service_idx ON postgres.songs_day_of_week_hour (played_at, artist_lower, service_id);
