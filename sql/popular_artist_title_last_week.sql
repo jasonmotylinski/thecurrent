8d553870b073ba68d2124a84b3e2f94bae179a9d
Jason Motylinski <jason@motylinski.com>
2025-12-28 17:06:52 -0500

    fix: error with SQL statement

--- sql/popular_artist_title_last_week.sql
+++ sql/popular_artist_title_last_week.sql
@@ -1,23 +1,18 @@
+
 SELECT 
-    artist, 
-    title, 
-    ct
-FROM (
-    SELECT 
-        MAX(artist) AS artist, 
-        MAX(title) AS title, 
-        artist_lower,
-        title_lower,
-        SUM(ct) as ct
-    FROM songs_day_of_week_hour
-    WHERE
-        service_id=%(service_id)s
-        AND played_at >= CURRENT_DATE - INTERVAL '7 DAY'
-        AND played_at <= CURRENT_DATE
-        AND artist_lower != '' AND title_lower != ''
-    GROUP BY 
-        artist_lower, 
-        title_lower
-    ORDER BY SUM(ct) DESC, MAX(artist) ASC
-    LIMIT 10
-) t1
+    MAX(artist) AS artist, 
+    MAX(title) AS title, 
+    artist_lower,
+    title_lower,
+    SUM(ct) as ct
+FROM songs_day_of_week_hour
+WHERE
+    service_id=%(service_id)s
+    AND played_at >= CURRENT_DATE - INTERVAL '7 DAY'
+    AND played_at <= CURRENT_DATE
+    AND artist_lower != '' AND title_lower != ''
+GROUP BY 
+    artist_lower, 
+    title_lower
+ORDER BY SUM(ct) DESC, MAX(artist) ASC
+LIMIT 10
--- sql/popular_artist_title_timeseries.sql
+++ sql/popular_artist_title_timeseries.sql
@@ -1,12 +1,17 @@
 WITH top_songs AS (
-    SELECT MAX(artist) AS artist, MAX(title) AS title, artist_lower, title_lower
+    SELECT 
+        MAX(artist) AS artist, 
+        MAX(title) AS title, 
+        artist_lower, 
+        title_lower,
+        COUNT(*) AS ct
     FROM songs_day_of_week_hour
     WHERE service_id = %(service_id)s
         AND played_at >= CURRENT_DATE - INTERVAL '7 DAY'
         AND played_at <= CURRENT_DATE
         AND artist_lower != '' AND title_lower != ''
     GROUP BY artist_lower, title_lower
-    ORDER BY SUM(ct) DESC
+    ORDER BY ct DESC, MAX(artist) ASC
     LIMIT 10
 ),
 weeks AS (
