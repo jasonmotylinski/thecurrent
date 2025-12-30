dad3c518c1b002e046353bc93d9b44306657449a
Jason Motylinski <jason@motylinski.com>
2025-12-30 14:39:13 -0500

    fix: incorrect column count for wext csv_task

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -184,24 +184,29 @@ class InsertDayData(luigi.Task):
                 con = sqlite3.connect(config.DB)
                 results=con.execute(t).fetchall()
                 if(len(results))==0:
-                    t="""INSERT INTO songs VALUES({id}, {artist}, {title}, {album}, {played_at}, {duration}, {service_id},
-                                                  {song_id}, {play_id}, {composer}, {conductor}, {orch_ensemble},
-                                                  {soloist_1}, {soloist_2}, {soloist_3}, {soloist_4},{soloist_5},
-                                                  {soloist_6}, {record_co}, {record_id}, {addl_text}, {broadcast}, 
-                                                  {songs_on_album},{songs_by_artist}, {album_mbid}, {art_url}, 
-                                                  {year}, {month}, {day}, {day_of_week}, {week},{hour})"""\
-                                                    .format(id=clean_str(row['id']),artist=clean_str(row['artist']),title=clean_str(row['title']),album=clean_str(row['album']),played_at=clean_str(row['played_at']),
-                                                            duration=clean_str(row['duration']),service_id=clean_str(row['service_id']), song_id=clean_str(row['song_id']),play_id=clean_str(row['play_id']),
-                                                            composer=clean_str(row['composer']), conductor=clean_str(row['conductor']), orch_ensemble=clean_str(row['orch_ensemble']),
-                                                            soloist_1=clean_str(row['soloist_1']),soloist_2=clean_str(row['soloist_2']),soloist_3=clean_str(row['soloist_3']),soloist_4=clean_str(row['soloist_4']),
-                                                            soloist_5=clean_str(row['soloist_5']),soloist_6=clean_str(row['soloist_6']),record_co=clean_str(row['record_co']),record_id=clean_str(row['record_id']),
-                                                            addl_text=clean_str(row['addl_text']),broadcast=clean_str(row['broadcast']), songs_on_album=clean_str(row['songs_on_album']),
-                                                            songs_by_artist=clean_str(row['songs_by_artist']), album_mbid=clean_str(row['album_mbid']), art_url=clean_str(row['art_url']),
-                                                            year=clean_str(row['year']), month=clean_str(row['month']), day=clean_str(row['day']), day_of_week=clean_str(row['day_of_week']), week=clean_str(row['week']),
-                                                            hour=clean_str(row['hour']))
-                    print(t)
-                    con.execute(t)
-                    con.commit()
+                    try:
+                        t="""INSERT INTO songs VALUES({id}, {artist}, {title}, {album}, {played_at}, {duration}, {service_id},
+                                                    {song_id}, {play_id}, {composer}, {conductor}, {orch_ensemble},
+                                                    {soloist_1}, {soloist_2}, {soloist_3}, {soloist_4},{soloist_5},
+                                                    {soloist_6}, {record_co}, {record_id}, {addl_text}, {broadcast}, 
+                                                    {songs_on_album},{songs_by_artist}, {album_mbid}, {art_url}, 
+                                                    {year}, {month}, {day}, {day_of_week}, {week},{hour})"""\
+                                                        .format(id=clean_str(row['id']),artist=clean_str(row['artist']),title=clean_str(row['title']),album=clean_str(row['album']),played_at=clean_str(row['played_at']),
+                                                                duration=clean_str(row['duration']),service_id=clean_str(row['service_id']), song_id=clean_str(row['song_id']),play_id=clean_str(row['play_id']),
+                                                                composer=clean_str(row['composer']), conductor=clean_str(row['conductor']), orch_ensemble=clean_str(row['orch_ensemble']),
+                                                                soloist_1=clean_str(row['soloist_1']),soloist_2=clean_str(row['soloist_2']),soloist_3=clean_str(row['soloist_3']),soloist_4=clean_str(row['soloist_4']),
+                                                                soloist_5=clean_str(row['soloist_5']),soloist_6=clean_str(row['soloist_6']),record_co=clean_str(row['record_co']),record_id=clean_str(row['record_id']),
+                                                                addl_text=clean_str(row['addl_text']),broadcast=clean_str(row['broadcast']), songs_on_album=clean_str(row['songs_on_album']),
+                                                                songs_by_artist=clean_str(row['songs_by_artist']), album_mbid=clean_str(row['album_mbid']), art_url=clean_str(row['art_url']),
+                                                                year=clean_str(row['year']), month=clean_str(row['month']), day=clean_str(row['day']), day_of_week=clean_str(row['day_of_week']), week=clean_str(row['week']),
+                                                                hour=clean_str(row['hour']))
+                        print(t)
+                        con.execute(t)
+                        con.commit()
+                    except Exception as e:
+                        print("Error inserting row: {0}".format(row))
+                        raise e
+                con.close()
 
 class InsertDayRangeData(luigi.Task):
     """Inserts the start to end date range of data into the databse"""
