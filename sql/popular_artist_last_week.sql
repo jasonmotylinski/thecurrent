69aa4b137f6c0272df1fd5ea653749534a11fff2
Jason Motylinski <jason@motylinski.com>
2026-01-02 08:36:46 -0500

    fix: revert last week query so that multiple titles are returned

--- sql/popular_artist_last_week.sql
+++ sql/popular_artist_last_week.sql
@@ -1,10 +1,23 @@
-
 SELECT 
     a.artist,
     a.title,
     COUNT(*) AS ct
 FROM
     songs_day_of_week_hour AS a
+INNER JOIN (
+    SELECT 
+        artist, 
+        COUNT(*) as ct
+    FROM songs_day_of_week_hour
+    WHERE 
+        service_id=%(service_id)s
+        AND played_at >= CURRENT_DATE - INTERVAL '7 DAY'
+        AND played_at <= CURRENT_DATE
+        AND (artist !='' AND title !='')
+    GROUP BY artist
+    ORDER BY ct DESC
+    LIMIT 10
+) AS b ON a.artist = b.artist
 WHERE
     a.service_id=%(service_id)s
     AND a.played_at >= CURRENT_DATE - INTERVAL '7 DAY'
@@ -12,7 +25,3 @@ WHERE
 GROUP BY 
     a.artist, 
     a.title
-ORDER BY
-    ct DESC,
-    artist ASC
-LIMIT 10
