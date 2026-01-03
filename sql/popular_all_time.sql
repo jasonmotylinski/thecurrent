4620a3222594bb968db51249ce0c799e21797618
Jason Motylinski <jason@motylinski.com>
2026-01-03 12:52:38 -0500

    fix: all time queries

--- sql/popular_all_time.sql
+++ sql/popular_all_time.sql
@@ -1,13 +1,14 @@
 SELECT 
-    artist, 
+    artist_normalized,
+    MAX(artist) AS artist,
     COUNT(*) AS ct 
 FROM songs_day_of_week_hour
 WHERE 
     service_id=%(service_id)s
-    AND artist != ''
-    AND title != ''
+    AND artist_normalized != ''
+    AND title_normalized != ''
 GROUP BY
     service_id,
-    artist
+    artist_normalized
 ORDER BY ct DESC
 LIMIT 5
