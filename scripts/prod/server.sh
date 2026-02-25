fb53be3a5dc5626ea1334f8d05f2f6242dec5474
Jason Motylinski <jason@motylinski.com>
2026-02-24 20:41:44 -0500

    feature: reduce workers

--- scripts/prod/server.sh
+++ scripts/prod/server.sh
@@ -1,3 +1,3 @@
 source venv/bin/activate
 export PYTHONPATH=.
-gunicorn --workers 7 --worker-class gevent --log-level DEBUG --bind unix:/run/thecurrent.sock dashboard.server:app
+gunicorn --workers 2 --worker-class gevent --log-level DEBUG --bind unix:/run/thecurrent.sock dashboard.server:app
